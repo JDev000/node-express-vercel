e9acbcf05fb4e9970c9de38115a9df7e5c83d079
JDev000 <jdeveloper2022@gmail.com>
2024-01-25 13:01:04 +0800

    updated

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-import express from 'express'
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("express vercel app response")
-})
+// Get the directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.listen(5000, ()=>{
-    console.log('app is running...')
-})
+// Import the 'path' module
+import path from 'path';
+
+// Define the path to your HTML file
+const htmlPath = path.join(__dirname, 'index.html');
+
+app.get('/', (req, res) => {
+    // Send the HTML file as the response
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(htmlPath);
+});
+
+app.listen(5000, () => {
+    console.log('App is running on http://localhost:5000');
+});
+
